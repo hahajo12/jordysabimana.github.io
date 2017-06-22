96bebb7e8c7a4c223f417fef34875c6de849d458
jordysabimana <jord.sab@gmail.com>
2017-06-22 10:20:00 +0200

    site update 2

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,57 @@
 /*===========================================================
                 SERVICES
 ===========================================================*/
-$(function() {
+$(function () {
     // animate on scroll
     new WOW().init();
 });
+
+
+/*====================================================
+                        NAVIGATION
+====================================================*/
+// Show/Hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+
+        } else {
+            // show nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+});
+// Smooth scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 64
+        }, 1250, "easeInOutExpo");
+    });
+});
+
+// Close mobile menu on click
+$(function () {
+
+    $(".navbar-collapse ul li a").on("click touch", function () {
+
+        $(".navbar-toggle").click();
+    });
+});
+
+
+
+
