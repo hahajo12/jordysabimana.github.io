83438499f6afb2d0eb4089949b31527cb58eb4a1
jordysabimana <jord.sab@gmail.com>
2022-09-24 17:00:19 +0200

    first commit

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,81 @@
                 SERVICES
 ===========================================================*/
 $(function () {
-    // animate on scroll
-    new WOW().init();
+  // animate on scroll
+  new WOW().init();
 });
 
+/*===========================================================
+                WORK
+===========================================================*/
+$(function () {
+
+  $("#work").magnificPopup({
+    delegate: 'a', // child items selector, by clicking on it popup will open
+    type: 'image',
+    gallery: {
+      enabled: true
+    }
+  });
+});
+
+/*===========================================================
+                TEAM
+===========================================================*/
+$(function () {
+
+  $("#team-members").owlCarousel({
+    items: 3,
+    autoplay: true,
+    smartspeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+
+});
+
+/*===========================================================
+                TESTIMONIALS
+===========================================================*/
+$(function () {
+
+  $("#customers-testimonials").owlCarousel({
+    items: 1,
+    autoplay: true,
+    smartspeed: 700,
+    loop: true,
+    autoplayHoverPause: true
+  });
+
+});
+
+/*====================================================
+                        CLIENTS
+====================================================*/
+$(function () {
+
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+		responsive: {
+		  0: {
+			items: 1
+		  },
+		  480: {
+			items: 3
+		  },
+		  768: {
+			items: 5
+		  },
+		  992: {
+			items: 6
+		  }
+		}
+    });
+});
 
 /*====================================================
                         NAVIGATION
@@ -44,10 +115,10 @@ $(function () {
 });
 
 // Close mobile menu on click
-$(function () {
-
-    $(".navbar-collapse ul li a").on("click touch", function () {
-
+$(function(){
+    
+    $(".navbar-collapse ul li a").on("click touch", function(){
+       
         $(".navbar-toggle").click();
     });
 });
